2b2864adad1dc11cf3ce77f1e2d1343cb1bb2c7a
skoshelev <sergey.k@nopcommerce.com>
2016-10-21 12:02:24 +0300

    Combine "Nop.Plugin.Tax.CountryStateZip" and "Nop.Plugin.Tax.FixedRate" plugins into single one

---
+++ src/Plugins/Nop.Plugin.Tax.FixedOrByCountryStateZip/CountryStateZipSettings.cs
@@ -0,0 +1,9 @@
+﻿using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Tax.FixedOrByCountryStateZip
+{
+    public class CountryStateZipSettings : ISettings
+    {
+        public bool Enabled { get; set; }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Tax.FixedOrByCountryStateZip/RouteProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Nop.Web.Framework.Mvc.Routes;
+
+namespace Nop.Plugin.Tax.FixedOrByCountryStateZip
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute("Plugin.Tax.FixedOrByCountryStateZip.AddTaxRate",
+                 "Plugins/FixedOrByCountryStateZip/AddTaxRate",
+                 new { controller = "FixedOrByCountryStateZip", action = "AddTaxRate" },
+                 new[] { "Nop.Plugin.Tax.FixedOrByCountryStateZip.Controllers" }
+            );
+        }
+        public int Priority
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
